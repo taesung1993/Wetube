4f97119816d845b1058d053f337b85fb1c1e45e4
taesung1993 <cheonyulin@gmail.com>
2020-07-28 20:33:06 +0900

    add video control function in frontend

--- assets/js/partials/videoPlayer.js
+++ assets/js/partials/videoPlayer.js
@@ -1,13 +1,19 @@
 const videoContainer = document.querySelector(".videoDetail__videoPlayer");
 const videoPlayer = document.querySelector("video");
+const videoControlBar = document.querySelector(".videoPlayer__control-bar");
 
 // 재생 관련 변수들
 const playBtn = document.getElementById("videoPlay");
 
 // 프로그래스 바 변수들
+const progressbarRange = document.querySelector(
+  ".videoPlayer__col:nth-child(2)"
+);
 const currentProgress = document.querySelector(".current-rate");
+const ctrlProgressbar = document.getElementById("currentRateBtn");
 
 // 볼륨 관련 변수들
+const volBtn = document.getElementById("volumeCtrl");
 const volArea = document.querySelector(".volume-control");
 const volTrack = document.querySelector(".volume-bar__track");
 const volCurrentLevel = document.querySelector(".volume-control .current-rate");
@@ -16,9 +22,39 @@ const volCtrlBtn = document.getElementById("volumeCtrlBtn");
 // 스크린 버튼 변수
 const screenCtrlBtn = document.getElementById("screenSizeCtrl");
 
+const videoThumbnails = [];
+let currentProgressBarRate = 0;
 let isVolCtrl = false;
+let isProgressCtrl = false;
+
+let curentVolLevel = 0.5;
+videoPlayer.volume = 0.5;
 
 // 볼륨 컨트롤 함수들
+
+const showVolCtrl = (event) => {
+  volArea.classList.remove("hidden");
+};
+
+const hideVolCtrl = () => {
+  volArea.classList.add("hidden");
+  isVolCtrl = false;
+};
+
+const handleMuted = () => {
+  const icon = document.querySelector("#volumeCtrl i");
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    icon.className = "fas fa-volume-up";
+    videoPlayer.volume = curentVolLevel;
+    volCurrentLevel.style.height = `${curentVolLevel * 100}%`;
+  } else {
+    videoPlayer.muted = true;
+    icon.className = "fas fa-volume-mute";
+    videoPlayer.volume = 0;
+    volCurrentLevel.style.height = "0%";
+  }
+};
 
 const endVolCtrl = () => {
   isVolCtrl = false;
@@ -32,6 +68,8 @@ const setVolLevel = (event) => {
     } else if (currentRate < 0) {
       currentRate = 0;
     }
+    curentVolLevel = currentRate / 100;
+    videoPlayer.volume = currentVolLevel;
     volCurrentLevel.style.height = `${currentRate}%`;
   }
 };
@@ -47,6 +85,19 @@ const handleVideoPlay = () => {
   } else if (videoPlayer.played) {
     videoPlayer.pause();
     icon.className = "fas fa-play";
+  }
+};
+
+const handleSpacebarPlay = (event) => {
+  const icon = document.querySelector("#videoPlay i");
+  if (event.keyCode == 32) {
+    if (videoPlayer.paused) {
+      videoPlayer.play();
+      icon.className = "fas fa-pause";
+    } else if (videoPlayer.played) {
+      videoPlayer.pause();
+      icon.className = "fas fa-play";
+    }
   }
 };
 
@@ -93,23 +144,105 @@ const updateVideoTime = () => {
   }
 };
 
+const goFullScreen = () => {
+  const icon = document.querySelector("#screenSizeCtrl i");
+  const exitFullScreen = () => {
+    icon.className = "fas fa-expand";
+    document.webkitExitFullscreen();
+    screenCtrlBtn.addEventListener("click", goFullScreen);
+  };
+  const handleExitScreen = () => {
+    // ESC로 전체화면 모드를 끌 때 발생하는 이벤트
+    if (!document.webkitIsFullScreen) {
+      icon.className = "fas fa-expand";
+      screenCtrlBtn.addEventListener("click", goFullScreen);
+    }
+  };
+  videoContainer.requestFullscreen();
+  icon.className = "fas fa-compress";
+  document.addEventListener("fullscreenchange", handleExitScreen);
+  screenCtrlBtn.addEventListener("click", exitFullScreen);
+  screenCtrlBtn.removeEventListener("click", goFullScreen);
+};
+
+const startProgressCtrl = () => {
+  isProgressCtrl = true;
+};
+
+const moveProgress = () => {
+  if (isProgressCtrl) {
+    const currentTimeEl = document.getElementById("currentTime");
+    const totalTime = videoPlayer.duration;
+    const currentTime = totalTime * currentProgressBarRate;
+    const currentTimeString = dateformat(currentTime);
+
+    videoPlayer.currentTime = currentTime;
+    currentTimeEl.innerText = currentTimeString;
+    isProgressCtrl = false;
+  }
+};
+
+const setProgressCtrl = (event) => {
+  isProgressCtrl = true;
+  if (isProgressCtrl) {
+    const start = parseInt(progressbarRange.getBoundingClientRect().left);
+    const end = parseInt(progressbarRange.getBoundingClientRect().width - 20);
+    let pointer = (start - event.clientX + 10) * -1;
+    currentProgressBarRate = (pointer / end).toFixed(2);
+    let percent = currentProgressBarRate * 100;
+
+    if (percent < 0) {
+      percent = 0;
+    } else if (percent > 100) {
+      percent = 100;
+    }
+
+    currentProgress.style.width = `${percent}%`;
+  }
+};
+
+const endProgressCtrl = () => {
+  isProgressCtrl = false;
+};
+
+const showVideoCtrl = () => {
+  videoControlBar.classList.remove("hidden");
+};
+
+const hideVideoCtrl = () => {
+  videoControlBar.classList.add("hidden");
+};
+
 const init = () => {
   // 비디오 로딩 함수
   videoPlayer.onloadedmetadata = handleLoading;
   videoPlayer.onloadedmetadata();
 
+  // 비디오 컨트롤 바 show/hide
+  videoContainer.addEventListener("mouseover", showVideoCtrl);
+  videoContainer.addEventListener("mouseleave", hideVideoCtrl);
+
   // 비디오 프로그래스 바 함수
   videoPlayer.addEventListener("timeupdate", updateVideoTime);
+  progressbarRange.addEventListener("mousedown", startProgressCtrl);
+  progressbarRange.addEventListener("mousemove", setProgressCtrl);
+  progressbarRange.addEventListener("mouseup", moveProgress);
+  progressbarRange.addEventListener("mouseleave", endProgressCtrl);
 
   // 비디오 플레이 함수
+  window.addEventListener("keydown", handleSpacebarPlay);
   playBtn.addEventListener("click", handleVideoPlay);
+
   // 볼륨 이벤트 함수
+  volBtn.addEventListener("mouseover", showVolCtrl);
+  volBtn.addEventListener("click", handleMuted);
   volArea.addEventListener("mousedown", startVolCtrl);
   volArea.addEventListener("mousemove", setVolLevel);
   volArea.addEventListener("mouseup", endVolCtrl);
-  volArea.addEventListener("mouseleave", endVolCtrl);
+  volArea.addEventListener("mouseleave", hideVolCtrl);
 
   // 스크린 관련 함수
+  screenCtrlBtn.addEventListener("click", goFullScreen);
 };
 
 if (videoContainer) {
